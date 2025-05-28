2b4752f93c15de40b61ac86f7914d565d01ad653
chanwoo <chanwoo.kim@snu.ac.kr>
2025-05-28 22:09:10 +0900

    [FIX] cases where cold items exist in test interactions

--- src/dataloader.py
+++ src/dataloader.py
@@ -4,38 +4,47 @@ import scipy.sparse as sp
 import torch
 from torch.utils.data import Dataset, DataLoader
 
-def build_interaction_matrix(path):
-    with open(path, "r") as f:
+def get_interaction(dataset):
+    '''
+    train_inter: scipy.sparse.coo_matrix of shape(num_users, num_items)
+    test_inter: dict of (key = user id, value = a set of item ids)
+    '''
+    train_path = f'./dataset/{dataset}/train.txt'
+    test_path = f'./dataset/{dataset}/test.txt'
+    max_uid, max_iid = -1, -1
+    
+    with open(train_path, "r") as f:
         lines = f.readlines()
     rows, cols = list(), list()
-    max_uid, max_iid = -1, -1
+    num_train = 0
     for line in lines:
         line = [int(idx) for idx in line.strip().split(" ")]
         uid, iids = line[0], line[1:] # first index is user id, the rest are item ids
         iids = list(set(iids)) # remove duplicates
         rows.extend([uid] * len(iids))
         cols.extend(iids)
+        num_train += len(iids)
         max_uid = max(max_uid, uid)
         max_iid = max(max_iid, *iids)
-    rows, cols = np.array(rows), np.array(cols)
-    vals = np.ones(len(rows), dtype=np.float32)
-    inter = sp.coo_matrix((vals, (rows, cols)), shape=(max_uid+1, max_iid+1))
-    num_inter = inter.count_nonzero()
-    return inter, num_inter # inter(scipy.sparse.coo_matrix) - (num_users, num_items)
 
-def load_interaction_dict(path):
-    with open(path, "r") as f:
+    with open(test_path, "r") as f:
         lines = f.readlines()
-    inter = dict()
-    num_inter = 0
+    test_inter = dict()
+    num_test = 0
     for line in lines:
         line = [int(idx) for idx in line.strip().split(" ")]
         uid, iids = line[0], line[1:] # first index is user id, the rest are item ids
         if len(iids) == 0: continue # skip users without interaction
         iids = set(iids) # remove duplicates
-        inter[uid] = iids
-        num_inter += len(iids)
-    return inter, num_inter # inter(dict) - (key = user id, value = a set of item ids)
+        test_inter[uid] = iids
+        num_test += len(iids)
+        max_uid = max(max_uid, uid)
+        max_iid = max(max_iid, *iids)
+        
+    rows, cols = np.array(rows), np.array(cols)
+    vals = np.ones(len(rows), dtype=np.float32)
+    train_inter = sp.coo_matrix((vals, (rows, cols)), shape=(max_uid+1, max_iid+1))
+    return train_inter, num_train, test_inter, num_test
 
 class AllRankData(Dataset):
     def __init__(self, observed_inter, test_inter):
@@ -58,8 +67,7 @@ def collate_fn(batch, device):
     return observed_inters, label_inters
 
 def load_data(dataset, batch_size, device):
-    train_inter, num_train = build_interaction_matrix(f'./dataset/{dataset}/train.txt')
-    test_inter , num_test  = load_interaction_dict(f'./dataset/{dataset}/test.txt')
+    train_inter, num_train, test_inter, num_test = get_interaction(dataset)
     test_data = AllRankData(train_inter, test_inter)
     test_dataloader = DataLoader(test_data,
                                  batch_size=batch_size,
